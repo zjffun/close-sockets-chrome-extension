4c8b1b8b3a91788194b1fd376ee75cd9b2737e53
JuFeng Zhang <zjffun@gmail.com>
2021-06-16 23:48:44 +0800

    feat: close sockets

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # close-sockets-chrome-extension
-Close idle sockets &amp;  Flush socket pools
+
+Close idle sockets &amp; Flush socket pools
+
+## Install
+
+**Note**: This plugin needs to execute script in [chrome://net-internals/#sockets](chrome://net-internals/#sockets). **Please make sure `Extensions on chrome:// URLs` enabled as step 5 to 6** to make this extension work.
+
+1. `git clone https://github.com/zjffun/close-sockets-chrome-extension.git`;
+2. Open [chrome://extensions/](chrome://extensions/);
+3. Click `Load unpacked`;
+4. Select `your-git-clone-path/close-sockets-chrome-extension`;
+5. Open [chrome://flags/#extensions-on-chrome-urls](chrome://flags/#extensions-on-chrome-urls);
+6. Select `enabled`.
+
+## Usage
+
+Click the extension icon <img src="./images/icon128.png" style="height: 1.5em"> , then this extension can help you:
+
+1. Open [chrome://net-internals/#sockets](chrome://net-internals/#sockets);
+2. Click `Close idle sockets` button;
+3. Click `Flush socket pools` button;
+4. Close tab opened in step 1.
---
+++ background.js
@@ -0,0 +1,47 @@
+function clickButton() {
+  document.querySelector("#sockets-view-close-idle-button").click();
+  document.querySelector("#sockets-view-flush-button").click();
+}
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+let createdTab;
+let currentTab;
+
+chrome.action.onClicked.addListener(function () {
+  getCurrentTab().then((tab) => {
+    console.log("action click", { tab });
+    currentTab = tab;
+    chrome.tabs.create(
+      { url: "chrome://net-internals/#sockets" },
+      function (tab) {
+        console.log("open chrome://net-internals/#sockets", { tab });
+        createdTab = tab;
+      }
+    );
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  console.log("tab updated", { tabId, changeInfo, tab });
+  if (
+    tabId === createdTab.id &&
+    changeInfo.status == "complete" &&
+    tab.active
+  ) {
+    chrome.scripting.executeScript(
+      {
+        target: { tabId },
+        function: clickButton,
+      },
+      function () {
+        chrome.tabs.remove(createdTab.id);
+        chrome.tabs.update(currentTab.id, { selected: true });
+      }
+    );
+  }
+});
